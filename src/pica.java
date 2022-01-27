efedfb1e322abccf854d478f24c9b2ab9464ffcd
pauls <gustavspauls1@gmail.com>
2022-01-27 17:03:08 +0200

    Pielaboji pasu kodu lai strada cita zara

--- src/pica.java
+++ src/pica.java
@@ -2,10 +2,27 @@
 import javax.swing.JOptionPane;
 public class pica {
 public static void picasveide() {
+	String picasVeids, topingi;
 	int izmers;
+	double cenaIzmers, cenaVeidu;
 	do {
 	izmers = Integer.parseInt(JOptionPane.showInputDialog("Kadu izmeru picu jus gribat?"));
 }while(izmers<20 || izmers>50);
+	cenaIzmers=izmers*0.05;
+	do {
+	picasVeids = JOptionPane.showInputDialog("Biezu,planu vai daudzgraudu?");
+	picasVeids = picasVeids.toLowerCase();
+	}while(!picasVeids.equals("biezu") && !picasVeids.equals("planu") && !picasVeids.equals("daudzgraudu"));
+	if(picasVeids.equals("biezu")) {
+		cenaVeidu = 3.58;
+} else if(picasVeids.equals("planu")) {
+	cenaVeidu = 7.32;
+}else {
+	cenaVeidu = 8.50;
+}
+	do {
+	topingi=JOptionPane.showInputDialog("stop to stop");
+}while(!topingi.equals("stop"));
 }
 	public static void main(String[] args) {
 				String izvele;
@@ -17,13 +34,11 @@ public static void picasveide() {
 					picasveide();
 					break;
 				case"stop":
-				
 				break;
 				default: JOptionPane.showMessageDialog(null,"Komanda nepastav", "Error", JOptionPane.ERROR_MESSAGE);
 				}
 				}while(!izvele.equals("stop"));
 
-			}
 		
 	}
 
